8cc51ce6274f73a0df30316fea6d5802afe11414
I ekwaL V <ekwal@protonmail.com>
2021-07-10 23:59:24 +0300

    feat: add scss variables rules

--- index.js
+++ index.js
@@ -1,4 +1,24 @@
 module.exports = {
   plugins: ['stylelint-scss'],
-  extends: ['stylelint-config-recommended', 'stylelint-config-recess-order']
+  extends: ['stylelint-config-recommended', 'stylelint-config-recess-order'],
+  rules: {
+    // Variables
+    'scss/dollar-variable-empty-line-before': [
+      'always',
+      {
+        except: ['first-nested', 'after-dollar-variable'],
+        ignore: ['after-comment'],
+        severity: 'error'
+      }
+    ],
+    'scss/dollar-variable-colon-newline-after': [
+      'always-multi-line',
+      { severity: 'error' }
+    ],
+    'scss/dollar-variable-colon-space-after': [
+      'always-single-line',
+      { severity: 'error' }
+    ],
+    'scss/dollar-variable-colon-space-before': ['never', { severity: 'error' }]
+  }
 }
