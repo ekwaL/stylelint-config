23c67c70014abcf4e15455b418e8e370ee20b7a6
I ekwaL V <ekwal@protonmail.com>
2022-08-11 22:44:02 +0300

    chore: update deps

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,3 +1,6 @@
 module.exports = {
-  extends: ['@ekwal/eslint-config-base', '@ekwal/eslint-config-prettier']
+  extends: ['@ekwal/eslint-config-base', '@ekwal/eslint-config-prettier'],
+  rules: {
+    'unicorn/no-null': 'off'
+  }
 }
